3fd8f452de939b18f3b79c5fd8c2b4e619a63ff1
Brian <xan.brian@gmail.com>
2020-11-18 09:14:02 +0800

    up

--- 平台版-insight_account.sql
+++ 平台版-insight_account.sql
@@ -5,21 +5,55 @@
 DROP TABLE IF EXISTS `iaf_account`;
 CREATE TABLE `iaf_account` (
   `id` char(32) NOT NULL COMMENT '主键(UUID)',
-  `tenant_id` char(32) NOT NULL COMMENT '租户ID',
-  `type` tinyint(3) unsigned NOT NULL COMMENT '类型:1.支付宝;2.微信',
+  `type` tinyint(3) unsigned NOT NULL COMMENT '类型:0.银行;1.支付宝;2.微信;3.店铺……',
+  `code` varchar(16) NOT NULL COMMENT '账号',
   `name` varchar(64) NOT NULL COMMENT '名称',
   `balance` decimal(18, 2) NOT NULL DEFAULT '0.00' COMMENT '账户余额',
+  `detail` json DEFAULT NULL COMMENT '账户信息',
+  `is_invalid` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否失效:0.有效;1.失效',
+  `creator` varchar(64) NOT NULL COMMENT '创建人',
+  `creator_id` char(32) NOT NULL COMMENT '创建人ID',
+  `created_time` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
-  KEY `idx_account_tenant_id` (`tenant_id`) USING BTREE
+  KEY `idx_account_code` (`code`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='资金账户表';
 
 -- ----------------------------
--- Table structure for iaf_account_check
+-- Table structure for iaf_account_detail
 -- ----------------------------
-DROP TABLE IF EXISTS `iaf_account_check`;
-CREATE TABLE `iaf_account_check` (
+DROP TABLE IF EXISTS `iaf_account_detail`;
+CREATE TABLE `iaf_account_detail` (
   `id` char(32) NOT NULL COMMENT '主键(UUID)',
   `account_id` char(32) NOT NULL COMMENT '资金账户ID',
+  `query_id` varchar(64) DEFAULT NULL COMMENT '交易查询ID',
+  `amount` decimal(18, 2) NOT NULL COMMENT '流水金额',
+  `remark` varchar(1024) DEFAULT NULL COMMENT '备注',
+  `status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '状态:0.待交易;1.交易成功;2.交易失败',
+  `detail` json DEFAULT NULL COMMENT '交易信息',
+  `created_time` datetime NOT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `idx_account_detail_account_id` (`account_id`) USING BTREE,
+  KEY `idx_account_detail_query_id` (`query_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='资金账户流水表';
+
+
+-- ----------------------------
+-- Table structure for iab_account
+-- ----------------------------
+DROP TABLE IF EXISTS `iab_account`;
+CREATE TABLE `iab_account` (
+  `id` char(32) NOT NULL COMMENT '主键(UUID),同租户ID',
+  `balance` decimal(18, 2) NOT NULL DEFAULT '0.00' COMMENT '账户余额',
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='商户账户表';
+
+-- ----------------------------
+-- Table structure for iab_account_check
+-- ----------------------------
+DROP TABLE IF EXISTS `iab_account_check`;
+CREATE TABLE `iab_account_check` (
+  `id` char(32) NOT NULL COMMENT '主键(UUID)',
+  `account_id` char(32) NOT NULL COMMENT '商户账户ID',
   `start_date` datetime NOT NULL COMMENT '开始日期',
   `end_date` datetime NOT NULL COMMENT '截止日期',
   `count` int(10) unsigned NOT NULL COMMENT '对账交易笔数',
@@ -34,29 +68,25 @@ CREATE TABLE `iaf_account_check` (
   `created_time` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
   KEY `idx_account_check_account_id` (`account_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='资金账户对账单表';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='商户账户对账单表';
 
 -- ----------------------------
--- Table structure for iaf_account_detail
+-- Table structure for iab_account_detail
 -- ----------------------------
-DROP TABLE IF EXISTS `iaf_account_detail`;
-CREATE TABLE `iaf_account_detail` (
+DROP TABLE IF EXISTS `iab_account_detail`;
+CREATE TABLE `iab_account_detail` (
   `id` char(32) NOT NULL COMMENT '主键(UUID)',
-  `account_id` char(32) NOT NULL COMMENT '资金账户ID',
+  `account_id` char(32) NOT NULL COMMENT '用户账户ID',
   `check_id` char(32) DEFAULT NULL COMMENT '对账单ID',
-  `business_code` varchar(32) NOT NULL COMMENT '业务单号',
+  `type` tinyint(3) NOT NULL COMMENT '类型:4.订单退款;5.售后赔付;7.订单收入;8.提现',
+  `business_code` varchar(32) DEFAULT NULL COMMENT '业务单号',
   `amount` decimal(18, 2) NOT NULL COMMENT '流水金额',
-  `detail` json DEFAULT NULL COMMENT '交易信息',
-  `query_id` varchar(64) DEFAULT NULL COMMENT '交易查询ID',
   `remark` varchar(1024) DEFAULT NULL COMMENT '备注',
-  `status` tinyint(3) unsigned NOT NULL DEFAULT '0' COMMENT '状态:0.待交易;1.交易成功;2.交易失败',
   `created_time` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
   KEY `idx_account_detail_account_id` (`account_id`) USING BTREE,
-  KEY `idx_account_detail_check_id` (`check_id`) USING BTREE,
-  KEY `idx_account_detail_business_code` (`business_code`) USING BTREE,
-  KEY `idx_account_detail_query_id` (`query_id`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='资金账户流水表';
+  KEY `idx_account_detail_business_code` (`business_code`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='商户账户流水表';
 
 
 -- ----------------------------
@@ -76,11 +106,10 @@ DROP TABLE IF EXISTS `iau_account_detail`;
 CREATE TABLE `iau_account_detail` (
   `id` char(32) NOT NULL COMMENT '主键(UUID)',
   `account_id` char(32) NOT NULL COMMENT '用户账户ID',
-  `type` tinyint(3) NOT NULL COMMENT '类型:0.订单收银;1.充值;4.业务退款;5.售后赔付;8.提现',
+  `type` tinyint(3) NOT NULL COMMENT '类型:0.订单收银;1.充值;4.订单退款;5.售后赔付;7.订单扣款',
   `business_code` varchar(32) DEFAULT NULL COMMENT '业务单号',
   `amount` decimal(18, 2) NOT NULL COMMENT '流水金额',
   `remark` varchar(1024) DEFAULT NULL COMMENT '备注',
-  `allow_invoice` bit(1) NOT NULL DEFAULT b'0' COMMENT '是否允许开票:0.不允许;1.允许',
   `created_time` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE,
   KEY `idx_account_detail_account_id` (`account_id`) USING BTREE,
--- 平台版-interface.sql
+++ 平台版-interface.sql
@@ -1,21 +1,21 @@
 INSERT `ici_interface`(`id`, `name`, `method`, `url`, `auth_code`, `limit_gap`, `limit_cycle`, `limit_max`, `message`, `is_verify`, `is_limit`, `created_time`) VALUES 
 -- 资金账户管理
-(replace(uuid(), '-', ''), '获取账户列表', 'GET', '/account/v1.0/accounts', 'getAccount', 1, NULL, NULL, NULL, 1, 1, now()),
+(replace(uuid(), '-', ''), '获取账户详情', 'GET', '/account/v1.0/accounts', 'getAccount', 1, NULL, NULL, NULL, 1, 1, now()),
 (replace(uuid(), '-', ''), '获取账户流水', 'GET', '/account/v1.0/accounts/records', 'getAccount', 1, NULL, NULL, NULL, 1, 1, now()),
 (replace(uuid(), '-', ''), '商户账户充值', 'PUT', '/account/v1.0/accounts/recharge', 'recharge', 1, NULL, NULL, NULL, 1, 1, now()),
 (replace(uuid(), '-', ''), '商户账户提现', 'PUT', '/account/v1.0/accounts/withdraw', 'withdraw', 1, NULL, NULL, NULL, 1, 1, now()),
-(replace(uuid(), '-', ''), '导出账户对账单', 'GET', '/account/v1.0/accounts/checks/{id}', 'getCheck', 1, NULL, NULL, NULL, 1, 1, now()),
+(replace(uuid(), '-', ''), '导出账户对账单', 'GET', '/account/v1.0/accounts/checks', 'getCheck', 1, NULL, NULL, NULL, 1, 1, now()),
 (replace(uuid(), '-', ''), '新建账户对账单', 'POST', '/account/v1.0/accounts/checks', 'newCheck', 1, NULL, NULL, NULL, 1, 1, now()),
 (replace(uuid(), '-', ''), '确认账户对账单', 'PUT', '/account/v1.0/accounts/checks', 'confirmCheck', 1, NULL, NULL, NULL, 1, 1, now()),
-
--- 在线结算
-(replace(uuid(), '-', ''), '支付宝回调', 'POST', '/account/callbacks/alipay', NULL, 0, NULL, NULL, NULL, 0, 0, now()),
-(replace(uuid(), '-', ''), '微信回调', 'POST', '/account/callbacks/wechat', NULL, 0, NULL, NULL, NULL, 0, 0, now()),
 
 -- 发票
 (replace(uuid(), '-', ''), '获取开票申请列表', 'GET', '/account/v1.0/invoices', 'getInvoice', 1, NULL, NULL, NULL, 1, 1, now()),
 (replace(uuid(), '-', ''), '获取开票申请详情', 'GET', '/account/v1.0/invoices/{id}', 'getInvoice', 1, NULL, NULL, NULL, 1, 1, now()),
 (replace(uuid(), '-', ''), '审核开票申请', 'PUT', '/account/v1.0/invoices/status', 'auditInvoice', 1, NULL, NULL, NULL, 1, 1, now()),
+
+-- 在线结算
+(replace(uuid(), '-', ''), '支付宝回调', 'POST', '/account/callback/alipay', NULL, 0, NULL, NULL, NULL, 0, 0, now()),
+(replace(uuid(), '-', ''), '微信回调', 'POST', '/account/callback/wechat', NULL, 0, NULL, NULL, NULL, 0, 0, now()),
 
 -- 客户端接口
 (replace(uuid(), '-', ''), '我的账户', 'GET', '/account/client/v1.0/accounts', NULL, 1, NULL, NULL, NULL, 1, 1, now()),
